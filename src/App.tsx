ea7901a98e78b676a45bbab9ad783c34c6334915
saya0118 <chemi0802@gmail.com>
2022-03-04 14:38:21 -0800

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Header } from './Header';
 
 export default function App() {
   return (
     <div>
-      <h1>Barebone</h1>
+      <Header />
     </div>
   );
 }
