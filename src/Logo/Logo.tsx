e294e31ec32af6c9cfc14972de8b5c977102f5d4
saya0118 <chemi0802@gmail.com>
2022-03-12 00:06:03 -0800

    removed empty line from Logo.tsx

--- src/Logo/Logo.tsx
+++ src/Logo/Logo.tsx
@@ -3,9 +3,5 @@ import styles from './Logo.module.css';
 // import { ReactComponent as SVGIcon } from '../../img/logo-starbucks.svg';
 
 export const Logo = () => {
-  return (
-    <div className={styles.logo}>
-      {/* <SVGIcon /> */}
-    </div>
-  );
+  return <div className={styles.logo}>{/* <SVGIcon /> */}</div>;
 };
