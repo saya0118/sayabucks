71ec88c1f7ec8c073008f297bedfed868790610d
saya0118 <chemi0802@gmail.com>
2022-03-27 01:41:38 -0700

    Added 'font-menu' css for hamburger menu page

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -4,11 +4,15 @@ import styles from './Item.module.css';
 type ItemProps = {
   text: string;
   url: string;
+  fontWeight: string;
 };
 
-export const Item = ({ text, url }: ItemProps) => {
+export const Item = ({ text, url, fontWeight }: ItemProps) => {
   return (
-    <a className={styles['items-list-text']} href={url}>
+    <a
+      className={`${styles[fontWeight]} ${styles['items-list-text']}`}
+      href={url}
+    >
       {text}
     </a>
   );
