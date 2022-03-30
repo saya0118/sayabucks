cb72c36010afa95cb351c8dbe989da9cc6ffba14
saya0118 <chemi0802@gmail.com>
2022-03-29 23:51:55 -0700

    added useState

--- src/Hamburger/Hamburger.tsx
+++ src/Hamburger/Hamburger.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Hamburger.module.css';
+
+const [open, setOpen] = useState(false);
 
 export const Hamburger = () => {
   return (
-    <div className={styles.navbar}>
-      <div className={styles.hamburger}>
+    <div className={styles.navbar} onClick={() => setOpen(!open)}>
+      <div className={`${styles.hamburger} ${open ? 'styles.active' : ''}`}>
         <span className={styles.bar}></span>
         <span className={styles.bar}></span>
         <span className={styles.bar}></span>
